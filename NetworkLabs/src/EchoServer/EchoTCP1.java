039c938334851d57308eb1fedc1ada0aff466d96
Oskar <Oskar@OskarsEnvy>
2014-04-01 11:52:44 +0200

    lab3

---
+++ NetworkLabs/src/EchoServer/EchoTCP1.java
@@ -0,0 +1,49 @@
+package EchoServer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoTCP1 {
+
+	public EchoTCP1() {
+		try {
+			ServerSocket serverSocket = new ServerSocket(30000);
+
+				Socket socket = serverSocket.accept();
+				InetAddress inetAddress = socket.getInetAddress();
+				System.out.println(inetAddress);
+				InputStream inputStream = socket.getInputStream();
+				OutputStream outputStream = socket.getOutputStream();
+				int readByte;
+				StringBuilder sb = new StringBuilder();
+				do {
+					readByte = inputStream.read();
+					char a = (char) readByte;
+					sb.append(a);
+					System.out.print(a);
+					if (a == '\n') {
+						readByte = -1;
+					}
+				} while (readByte != -1);
+
+				byte[] output = sb.toString().getBytes();
+
+				outputStream.write(output);
+				outputStream.flush();
+				outputStream.close();
+				inputStream.close();
+				socket.close();
+				serverSocket.close();	
+				
+			
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ NetworkLabs/src/EchoServer/EnMain.java
@@ -0,0 +1,15 @@
+package EchoServer;
+
+
+public class EnMain {
+
+	/**
+	 * @param Hej
+	 */
+	public static void main(String[] args) {
+
+		EchoTCP1 server = new EchoTCP1();
+
+	}
+
+}
