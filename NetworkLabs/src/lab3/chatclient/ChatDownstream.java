493ea26c86be6d25697dad0be57aeba925846771
atf10aso <atf10aso@panter-3.student.lth.se>
2014-04-08 11:03:21 +0200

    ChatDownstream

---
+++ NetworkLabs/src/lab3/chatclient/ChatDownstream.java
@@ -0,0 +1,40 @@
+package lab3.chatclient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class ChatDownstream extends Thread {
+	
+	private InputStream in;
+	public ChatDownstream(Socket socket) throws IOException{
+		in = socket.getInputStream();
+	}
+	
+	public void run(){
+		while(true){
+			try {
+				String s = readNextMessage();
+				System.out.println(s);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public String readNextMessage() throws IOException{
+		int readByte;
+		StringBuilder sb = new StringBuilder();
+		do {
+			readByte = in.read();
+			char a = (char) readByte;
+			sb.append(a);
+			System.out.print(a);
+			if (a == '\n') {
+				readByte = -1;
+			}
+		} while (readByte != -1);
+		
+		return sb.toString();
+	}
+}
