dcf64e9ccf147f6e54252f0fc3758c601ba20368
atf10aso <atf10aso@panter-3.student.lth.se>
2014-04-08 10:55:30 +0200

    I lieak snow

---
+++ NetworkLabs/src/lab3/serverIntegration/ChatUpstream.java
@@ -0,0 +1,31 @@
+package lab3.serverIntegration;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatUpstream extends Thread {
+	private OutputStream os;
+	private Socket socket;
+
+	public ChatUpstream(Socket socket) throws IOException {
+		os = socket.getOutputStream();
+		this.socket = socket;
+	}
+
+	public void run() {
+		Scanner scan = new Scanner(System.in);
+		try {
+			while (scan.hasNextLine()) {
+				String input = scan.nextLine();
+				if (input.equals("close")) break;
+				os.write(input.getBytes());
+			}
+			socket.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
