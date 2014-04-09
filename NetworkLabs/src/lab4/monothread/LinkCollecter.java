8d0d4023c30c26eb4f6290205c2624f64bf88af1
AAAton <anton@monokulturen.se>
2014-04-09 18:30:58 +0200

    Finished part1 of Lab 4

--- NetworkLabs/src/lab4/monothread/LinkCollecter.java
+++ NetworkLabs/src/lab4/monothread/LinkCollecter.java
@@ -4,6 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -13,27 +14,27 @@ import javax.swing.text.html.HTMLEditorKit;
 public class LinkCollecter {
 	private URL baseUrl;
 	private static ArrayList<URL> urls,queue,emails;
-	
+
 	public LinkCollecter(URL url){
 		this.baseUrl = url;
 		queue = new ArrayList<URL>();
 		urls = new ArrayList<URL>();
 		emails = new ArrayList<URL>();
-		
+
 	}
 
 	public void getLinksBreadthFirst(int maximum) {		
 		queue.add(baseUrl);
-		
+
 		while(urls.size()<maximum){
 			URL url = getFromHeadOfQueue();
 			if(url==null) break;
-			
+
 			//TODO: Check mime-type!
 			findLinks(url);
 		}
 	}
-	
+
 	public ArrayList<URL> getEmailAddresses(){
 		return emails;
 	}
@@ -49,25 +50,34 @@ public class LinkCollecter {
 	}
 
 	private void findLinks(URL parent) {
-		
+
 		System.out.println("Fetching links from "+parent);
-        ParserGetter kit = new ParserGetter();
-        HTMLEditorKit.Parser parser = kit.getParser();
-        HTMLEditorKit.ParserCallback callback = new LinkGetterModified(parent);
-        
-        try {
-            InputStream in = new BufferedInputStream(parent.openStream());
-            InputStreamReader r = new InputStreamReader(in);
-            parser.parse(r, callback, true);
-        } catch(java.io.FileNotFoundException e){
-        	System.out.println("\t404 on "+parent+", removing from list...");
-        	urls.remove(parent);
-        }catch (IOException ex) {
-            ex.printStackTrace();
-            System.err.println(ex);
-        }
+		ParserGetter kit = new ParserGetter();
+		HTMLEditorKit.Parser parser = kit.getParser();
+		HTMLEditorKit.ParserCallback callback = new LinkGetterModified(parent);
+
+		try {
+			HttpURLConnection conn = (HttpURLConnection) parent.openConnection();
+			conn.setRequestMethod("HEAD");
+			conn.connect();
+			if(isValidMIMEType(conn.getContentType())){
+				InputStream in = new BufferedInputStream(parent.openStream());
+				InputStreamReader r = new InputStreamReader(in);
+				parser.parse(r, callback, true);
+			}
+		} catch(java.io.FileNotFoundException e){
+			System.out.println("\t404 on "+parent+", removing from list...");
+			urls.remove(parent);
+		}catch (IOException ex) {
+			ex.printStackTrace();
+			System.err.println(ex);
+		}
 	}
-	
+
+	private boolean isValidMIMEType(String contentType) {
+		return contentType.contains("text/html");
+	}
+
 	public static void addLinkToQueue(URL url,URL parent){	
 		if(!queue.contains(url) && !urls.contains(url)){ //Have we been here before?
 			queue.add(url);
@@ -77,8 +87,9 @@ public class LinkCollecter {
 	}
 
 	public static void addMailAddress(String href) throws MalformedURLException {
-		if(!emails.contains(href))
-			emails.add(new URL(href));
+		URL url = new URL(href);
+		if(!emails.contains(url))
+			emails.add(url);
 		System.out.println("\t"+href+" was added to emails");
 	}
 }
--- NetworkLabs/src/lab4/monothread/MonoThread.java
+++ NetworkLabs/src/lab4/monothread/MonoThread.java
@@ -1,6 +1,12 @@
 package lab4.monothread;
 
 import java.util.ArrayList;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -13,16 +19,37 @@ public class MonoThread {
 			URL baseUrl = new URL(BASE_URL);
 			LinkCollecter linkCollecter = new LinkCollecter(baseUrl);
 
-			linkCollecter.getLinksBreadthFirst(1000);
+			linkCollecter.getLinksBreadthFirst(100);
 			ArrayList<URL> links,emails;
 			links = linkCollecter.getLinks();
 			emails = linkCollecter.getEmailAddresses();
 			
-			System.out.println(links.size());
-			System.out.println(emails.size());
-			
+			writeToFile(links,"links.txt");
+			writeToFile(emails,"emails.txt");
+		
 		} catch (MalformedURLException e) {
 			System.out.println("Your base-url is dumb");
+		}
+	}
+
+	private static void writeToFile(ArrayList<URL> links, String filename) {
+		
+		File folder = new File("links");
+		if(!folder.exists()) folder.mkdir();
+		
+		File file = new File(folder,filename);
+		try {
+			FileWriter fileWriter = new FileWriter(file);
+			for(URL link:links){
+				fileWriter.write(link.toString());
+				fileWriter.write('\n');
+			}
+			fileWriter.flush();
+			fileWriter.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
